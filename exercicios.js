372ad90cbfc1dbd49aa3156b324f096162cdabc2
Luiza Timporini <luizatimporinifrazao@gmail.com>
2026-02-23 14:47:31 -0300

    Add files via upload

---
+++ exercicios.js
@@ -0,0 +1,199 @@
+//ex 1
+
+let num = "3"
+if (num >= "0"){
+    console.log("positivo")
+} else{
+    console.log("negativo")
+}
+
+//////////////////////////////////////////////
+
+//ex 2
+
+let idade = 18;
+
+if (idade >=18){
+    console.log("você é maior de idade")
+} else{
+    console.log("voc~e é menor de idade")
+
+}
+
+
+//////////////////////////////////////////////
+
+
+//ex 3
+
+let nume = 3;
+if (nume % 2 === 0){
+    console.log("PAR")
+} else{
+    console.log("IMPAR")
+}
+
+//////////////////////////////////////////////
+
+//ex 4
+
+let nota = 8;
+if (nota >= 6){
+    console.log("aprovado!")
+}
+else{
+    console.log("reprovado")
+}
+
+
+//////////////////////////////////////////////
+
+//ex 5
+
+let idd = 16;
+
+if (idd >=16){
+    console.log("você pode votar")
+} else{
+    console.log("você não pode votar")
+
+}
+
+
+//////////////////////////////////////////////
+
+//ex 6
+let senha = "1234"
+if (senha=== "1234"){
+    console.log("acesso perrmitido")
+} else{
+    console.log("acesso negado")
+}
+
+
+//////////////////////////////////////////////
+
+//ex 7
+let temperatra = "27"
+if (temperatra >= "25"){
+    console.log( "Está quente")
+} else{
+    console.log( "Está frio")
+}
+//////////////////////////////////////////////
+
+//ex 8
+
+let altura= 1.60;
+
+if (altura >=1.30){
+    console.log("permitido")
+} else{
+    console.log("não permitido")
+
+}
+
+
+//////////////////////////////////////////////
+
+//ex 9
+
+let numer = "3"
+if (numer >= "0"){
+    if (numer===0){
+    console.log("zer")
+} else{
+    console.log("positivo")
+}} 
+else{
+    console.log("negativo")
+}
+
+//////////////////////////////////////////////
+
+//ex 10
+let notas = 8;
+if (notas >= 6){
+    if (notas === 0 ){
+         console.log("Regular")
+    }
+
+    else if (notas === 7 || notas === 8){
+         console.log("Bom")
+    }
+
+    else if (notas === 9 ){
+         console.log("Excelente")
+    }
+    
+    
+}
+else{
+    console.log("reprovado")
+}
+
+//////////////////////////////////////////////
+
+//ex 11
+
+let age=20;
+let temCNH=true;
+
+if(age >= 18 && temCNH){
+    console.log("pode dirigir")
+} else{
+    console.log("não pode dirigir")
+}
+
+//////////////////////////////////////////////
+
+//ex 12
+
+let compra= 1190;
+
+if (compra >=100){
+    console.log("permitido")
+} else{
+    console.log("não permitido")
+}
+
+
+//////////////////////////////////////////////
+
+//ex 13
+let user = "Adimin"
+let keyword = "123"
+if (keyword=== "123" && user === "Adimin"){
+    console.log("acesso perrmitido")
+} else{
+    console.log("acesso negado")
+}
+
+
+//////////////////////////////////////////////
+
+//ex 14
+
+let filme = 18;
+if (filme>=16){
+    console.log("Você pode assistir o filme: Jogos Vorazes")
+} else{
+    console.log("Você não pode assistir o filme: Jogos Vorazes")
+}
+
+
+//////////////////////////////////////////////
+
+//ex 15
+
+let cor= "amarelo";
+
+if (cor === "verde"){
+    console.log(" pode passar")
+}
+ else if (cor === "amarelo") {
+    console.log("atenção")
+ }
+else{
+    console.log("pare")
+}
